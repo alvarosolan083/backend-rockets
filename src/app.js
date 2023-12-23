56cfbd08eaa470fca1def7deb282316c3dcc57dd
TuNombre <tuemail@example.com>
2023-12-23 10:57:11 -0300

    Se mejora la estructura de las carpetas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
-const db = require('./db'); 
+const db = require('./config/db.js'); 
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Ruta para obtener todos los árboles
 app.get('/arboles', async (req, res) => {
@@ -38,11 +53,28 @@ app.get('/arboles/:arbol_id/fotos', async (req, res) => {
 // Ruta para agregar un comentario a un árbol
 app.post('/arboles/:arbol_id/comentarios', async (req, res) => {
   const { arbol_id } = req.params;
-  const { comentario, postulante_id, linkedin, github_tarea, telefone, otras_referencias } = req.body;
+  const { postulante_id, comentario } = req.body; 
+
   try {
-    const result = await db.query('INSERT INTO roket.comentarios (arbol_id, comentario, postulante_id, linkedin, github_tarea, telefone, otras_referencias) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-      [arbol_id, comentario, postulante_id, linkedin, github_tarea, telefone, otras_referencias]);
-    res.json(result.rows);
+    // Insertar el nuevo comentario en la base de datos
+    const insertQuery = `
+      INSERT INTO roket.comentarios (arbol_id, postulante_id, comentario)
+      VALUES ($1, $2, $3)
+    `;
+    await db.query(insertQuery, [arbol_id, postulante_id, comentario]);
+
+    res.status(201).json({ message: 'Comentario agregado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Ruta para obtener los comentarios de un árbol por su ID
+app.get('/arboles/:arbol_id/comentarios', async (req, res) => {
+  const { arbol_id } = req.params;
+  try {
+    const comentarios = await db.query('SELECT * FROM roket.comentarios WHERE arbol_id = $1', [arbol_id]);
+    res.json(comentarios.rows);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -53,8 +85,7 @@ app.get('/', (req, res) => {
   res.send('¡Hola! Esta es la página principal.');
 });
 
-// Definición del puerto y escucha del servidor
-const PORT = process.env.PORT || 3000;
+// Escucha del servidor en el puerto definido
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
